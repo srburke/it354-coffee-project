aff7c56db053bfe0f3e8637e8baf1f84b4e3fc70
srburke <shayray601@gmail.com>
2023-03-22 19:43:12 -0500

    Added Basic Components and Router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
- import Header from './components/Header'
+import NavBar from './components/NavBar';
+import Home from './components/Home';
+import Coffee from './components/Coffee';
+import Single from './components/Single';
+import Account from './components/Account';
 
 function App() {
   return (
-    <div>
-    <Header />
-    </div>
-    <div>
+    <>
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/coffee' element={<Coffee />} />
+        <Route path='/single' element={<Single />} />
+        <Route path='/account' element={<Account />} />
+      </Routes>
       <Main />
       <Basket />
-    </div>
+    </>
   );
 }
 
