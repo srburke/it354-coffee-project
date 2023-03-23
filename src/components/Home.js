48f064fcb2447d2d3840258ba5e89c3794133ad4
Andrea  Lynn <andrealynn@Andreas-MacBook-Air.local>
2023-03-23 16:18:45 -0500

    added content to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,9 +6,17 @@ const Home = () => {
         <div className="container-fluid" id="home">
             <div className='container'>
                 <div className="row justify-content-center align-items-center g-2">
-                    <div className="col">Column</div>
-                    <div className="col">Column</div>
-                    <div className="col">Column</div>
+                    <div className="col">
+                        
+                        <h2>Coffee Beans</h2>
+                        <p>Browse our variety of coffee beans from all your favorite brands:</p>
+                            <ul>
+                                <li>Starbucks</li>
+                                <li>Dunkin</li>
+                                <li>Caribou Coffee</li>
+                            </ul>
+                        </div>
+                    <div className="col">Single Serve Cups</div>
                 </div>
             </div>
         </div>
